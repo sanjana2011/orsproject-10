acf3859e72352b5d0da4ff123f0cda8578e647a0
sanjana_2011 <sanjanagangrade20816@acropolis.in>
2026-05-26 22:48:34 +0530

    done

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -12,8 +12,6 @@ export class EventComponent extends BaseCtl {
   constructor(public locator: ServiceLocatorService, route: ActivatedRoute) {
     super(locator.endpoints.EVENT, locator, route);
      }
-<<<<<<< HEAD
-     
-=======
->>>>>>> d29b312e156c52aa1f5f5f281e451d8da176b856
+
+
 } 
