29a2f771f692ecc124ab837cc630537e7acd9c66
Sanjana Gangrade <87362088+sanjana2011@users.noreply.github.com>
2026-05-27 12:15:38 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './login/sign-up.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserComponent } from './user/user.component';
+import { UserComponent } from './user/user.component'; 
 import { UserListComponent } from './user/user-list.component';
 import { RoleComponent } from './role/role.component';
 import { CollegeComponent } from './college/college.component';
